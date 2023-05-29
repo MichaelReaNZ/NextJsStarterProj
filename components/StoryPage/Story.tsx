a2361a7184ee67efb3b6c204a292a07c3bfc22c0
MichaelReaNZ <MichaelReaNZ@gmail.com>
2023-05-29 18:14:39 +1200

    Minor UI changes, scroll to latest msg

--- components/StoryPage/Story.tsx
+++ components/StoryPage/Story.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Message, Story } from "@prisma/client";
 import MessageComponent from "./Message";
 import MessageInput from "./MessageInput";
@@ -12,15 +12,19 @@ type Props = {
 
 function Story({ story, messages: initialMessages }: Props) {
   const [messages, setMessages] = useState<Message[]>(initialMessages);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
 
   const addMessage = (newMessage: Message) => {
     setMessages([...messages, newMessage]);
   };
 
   return (
-    <div className="flex h-screen">
+    <div className="flex flex-col h-screen">
       <div className="flex-1 overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200">
-        {/* <div className="flex-1 overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200"> */}
         <div className="header text-lg text-center py-5 text-white">
           <h1>Story ID: {story.id}</h1>
           <h3>Story Name: {story.name}</h3>
@@ -30,10 +34,11 @@ function Story({ story, messages: initialMessages }: Props) {
           {messages.map((message) => (
             <MessageComponent key={message.id} message={message} />
           ))}
+          <div ref={messagesEndRef} />
         </div>
-
-        <MessageInput storyId={story.id} addMessage={addMessage} />
       </div>
+
+      <MessageInput storyId={story.id} addMessage={addMessage} />
     </div>
   );
 }
